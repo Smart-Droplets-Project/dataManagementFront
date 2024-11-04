6e46f97e9bbbccd32647243b0c185806e6a403f3
Mateo Budanovic <mateo.budanovic@vizlore.com>
2024-11-04 12:02:27 +0100

    grid layout changes and unnecessary fonts removed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,18 @@
 // app/dashboard/page.tsx
 import ParcelList from "@/app/components/ParcelList";
 import { Typography } from "@mui/material";
+import GridLayout from "../components/GridLayout";
 
 const DashboardPage = () => {
     return (
-        <div className="flex flex-col gap-4">
-            <Typography variant="h2" component="div">
-                Parcel Dashboard
-            </Typography>
-            <ParcelList />
-        </div>
+        <GridLayout>
+            <div className="flex flex-col gap-4">
+                <Typography variant="h2" component="div">
+                    Parcel Dashboard
+                </Typography>
+                <ParcelList />
+            </div>
+        </GridLayout>
     );
 };
 
