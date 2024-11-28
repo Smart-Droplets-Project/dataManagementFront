c46a1dcfcf2cf4589f1aab7c4a33d3da86e95f86
Mateo Budanovic <mateo.budanovic@vizlore.com>
2024-11-28 12:12:38 +0100

    removed nextjs footer from the home page

--- app/page.tsx
+++ app/page.tsx
@@ -20,60 +20,6 @@ export default function Home() {
             Welcome to the Smart Droplets Dashboard!
           </Typography>
         </div>
-
-        <footer className="flex justify-center gap-10">
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex flex-col items-center justify-center"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/file.svg"
-              alt="File icon"
-              width={24}
-              height={16}
-            />
-            <Typography variant="h6">
-              Learn
-            </Typography>
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex flex-col items-center justify-center"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/window.svg"
-              alt="Window icon"
-              width={24}
-              height={16}
-            />
-            <Typography variant="h6">
-              Examples
-            </Typography>
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex flex-col items-center justify-center"
-          >
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/globe.svg"
-              alt="Globe icon"
-              width={24}
-              height={16}
-            />
-            <Typography variant="h6">
-              Go to nextjs.org →
-            </Typography>
-          </a>
-        </footer>
       </div>
     </GridLayout>
   );
