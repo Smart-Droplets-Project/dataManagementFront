8baa3f87e527da5bff426402fe0ff3b209ffb36e
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-01-23 14:17:34 +0100

    moved fetch parcels logic to separate lib file and used it on server pages to get data on server

--- src/app/(dashboard)/parcels/page.tsx
+++ src/app/(dashboard)/parcels/page.tsx
@@ -2,12 +2,7 @@ import { Box } from "@mui/material";
 import { Suspense } from "react";
 import Loading from "./loading";
 import ParcelList from "@/components/ParcelList";
-import { AgriParcel } from "@/lib/interfaces";
-
-async function fetchParcels(): Promise<AgriParcel[]> {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/parcels`, { cache: 'no-store' });
-    return res.json();
-}
+import { fetchParcels } from "@/lib/fetch/fetchParcels";
 
 export default async function Home() {
     const parcels = await fetchParcels();
--- src/app/(map)/parcels/[id]/page.tsx
+++ src/app/(map)/parcels/[id]/page.tsx
@@ -1,14 +1,9 @@
-import { AgriParcel } from '@/lib/interfaces';
 import { ParcelDrawerProvider } from '@/contexts/ParcelDrawerContext';
 import ParcelDrawer from '@/components/ParcelDrawer/ParcelDrawer';
 import { Suspense } from 'react';
 import Loading from './loading';
 import ParcelMapWrapper from '@/components/ParcelMap/ParcelMapWrapper';
-
-async function fetchParcels(): Promise<AgriParcel[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/parcels`, { cache: 'no-store' });
-  return res.json();
-}
+import { fetchParcels } from '@/lib/fetch/fetchParcels';
 
 export default async function ParcelPage(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
