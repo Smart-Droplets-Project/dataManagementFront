f224ccaf13738f0cb56f4ba1e9af3c2de5f37502
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-03-04 12:09:33 +0100

    refactored loading and error pages for map routes

--- src/app/(map)/parcels/[id]/loading.tsx
+++
@@ -1,7 +0,0 @@
-import { Skeleton } from "@mui/material";
-
-export default function Loading() {
-    return (
-        <Skeleton sx={{ transform: "none", margin: 3, height: "100%" }} />
-    );
-}
--- src/app/(map)/parcels/[id]/page.tsx
+++ src/app/(map)/parcels/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import Loading from './loading';
+import Loading from '../../loading';
 import ParcelMapWrapper from '@/components/ParcelMap/ParcelMapWrapper';
 import { fetchParcels } from '@/lib/fetch/fetchParcels';
 
