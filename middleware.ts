334b13bb74c2cdb5cf2eca5f9447e076d40d3998
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-04-03 15:49:01 +0200

    created auth lib file which hosts auth route logic and updated middleware and server side fetch parcels file to append auth token to header

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,25 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-export function middleware(req: NextRequest): NextResponse {
-  const response = NextResponse.next();
+export async function middleware(req: NextRequest) {
+  const requestHeaders = new Headers(req.headers);
+  
+  // Add Cache-Control for API routes
+  if (req.nextUrl.pathname.startsWith("/api/")) {
+    requestHeaders.set("Cache-Control", "no-store, max-age=0");
 
-  // Add headers for API routes
-  if (req.nextUrl.pathname.startsWith('/api/')) {
-    response.headers.set('Cache-Control', 'no-store, max-age=0');
+    // Get the user token from the session
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+    if (token?.accessToken) {
+      requestHeaders.set("Authorization", `Bearer ${token.accessToken}`);
+    }
   }
 
-  return response;
+  return NextResponse.next({ request: { headers: requestHeaders } });
 }
+
+export const config = {
+  matcher: "/api/:path*", // Only applies to API routes
+};
--- src/lib/fetch/fetchParcels.ts
+++ src/lib/fetch/fetchParcels.ts
@@ -1,22 +1,28 @@
-import { ENDPOINTS, CONTEXTS } from '@/lib/constants';
-import { AgriParcel } from '@/lib/interfaces';
+import { ENDPOINTS, CONTEXTS } from "@/lib/constants";
+import { AgriParcel } from "@/lib/interfaces";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export async function fetchParcels(): Promise<AgriParcel[]> {
+  const session = await getServerSession(authOptions);
+  const token = session?.user?.accessToken;
+
   const searchParams = {
-    type: 'AgriParcel',
-    limit: '10',
+    type: "AgriParcel",
+    limit: "10",
   };
 
   const urlQuery = new URLSearchParams(searchParams).toString();
-  const url = new URL(`${ENDPOINTS.API_BASE_URL}?${urlQuery}`).toString();
+  const url = `${ENDPOINTS.API_BASE_URL}?${urlQuery}`;
 
   const response = await fetch(url, {
     headers: {
       Link: CONTEXTS.AGRIFARM,
+      Authorization: token ? `Bearer ${token}` : "",
     },
-    cache: 'no-store'
+    cache: "no-store",
   });
 
   if (!response.ok) {
