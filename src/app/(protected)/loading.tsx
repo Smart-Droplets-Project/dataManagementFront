8c68b412c0f391ca34342b9e584f4be3e20de5cb
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-04-03 14:43:27 +0200

    loading for protected layout styled

--- src/app/(protected)/loading.tsx
+++ src/app/(protected)/loading.tsx
@@ -1,7 +1,9 @@
-import { Skeleton } from "@mui/material";
+import { Box, Skeleton } from "@mui/material";
 
 export default function Loading() {
     return (
-        <Skeleton sx={{ transform: "none", height: "100%" }} />
+        <Box sx={{ padding: 3, height: "100%" }}>
+            <Skeleton sx={{ transform: "none",  height: "100%" }} />
+        </Box>
     );
 }
