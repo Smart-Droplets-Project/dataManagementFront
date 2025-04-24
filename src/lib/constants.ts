b53f4e4745709683a47e2a65bc00c48d147e14df
Mihailo Ilic <mihailo.ilic@vizlore.com>
2025-04-24 14:55:10 +0200

    fix: Fixed URL to include proxy endpoint

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,11 +3,10 @@ const NGSI_ENTITY_QUERY_PATH = "ngsi-ld/v1/entities";
 
 
 
-// TODO: These 2 localhost values need to be exchanged to "oauth2-proxy:4180"
 export const ENDPOINTS = {
     // API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || `http://localhost:1026/${NGSI_ENTITY_QUERY_PATH}`,
-    API_BASE_URL: process.env.ORION_CB_BASE_URL ? `${process.env.ORION_CB_BASE_URL}/${NGSI_ENTITY_QUERY_PATH}` : `http://localhost:1026/${NGSI_ENTITY_QUERY_PATH}`,
-    QUANTUMLEAP_URL: process.env.QUANTUMLEAP_URL ? `${process.env.QUANTUMLEAP_URL}/v2` : 'http://localhost:8668/v2',
+    API_BASE_URL: process.env.ORION_CB_BASE_URL ? `${process.env.ORION_CB_BASE_URL}/${NGSI_ENTITY_QUERY_PATH}` : `http://oauth2-proxy:4180/${NGSI_ENTITY_QUERY_PATH}`,
+    QUANTUMLEAP_URL: process.env.QUANTUMLEAP_URL ? `${process.env.QUANTUMLEAP_URL}/v2` : 'http://oauth2-proxy:4180/v2',
 };
 
 const REALM_NAME = "smart-droplets-realm";
