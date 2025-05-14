f12805d09f247e9e79409ebbe7e778eadc2e44a5
Nikola Letvenčuk <letvencuk.ra93.2021@uns.ac.rs>
2025-05-14 23:01:45 +0200

    feat: added weatherForecast for Parcel feature

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -24,5 +24,6 @@ export const CONTEXTS = {
     AGRIFARM: '<https://raw.githubusercontent.com/smart-data-models/dataModel.AgriFood/master/context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"',
     CORE: '<https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld>',
     DEVICE: '<https://raw.githubusercontent.com/smart-data-models/dataModel.Device/master/context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"',
-    AUTONOMOUSMOBILEROBOT: '<https://raw.githubusercontent.com/smart-data-models/dataModel.AutonomousMobileRobot/master/context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"'
+    AUTONOMOUSMOBILEROBOT: '<https://raw.githubusercontent.com/smart-data-models/dataModel.AutonomousMobileRobot/master/context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"',
+    WEATHERFORECAST: '<https://raw.githubusercontent.com/smart-data-models/dataModel.Weather/master/context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"'
 }
--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -161,3 +161,203 @@ export interface StateMessage {
         remainingPercentage: number
     }
 }
+
+export interface AgriParcel {
+    id: string;
+    type: string;
+    name?: {
+        type: string;
+        value: string;
+    };
+    address?: {
+        type: string;
+        value: string;
+    },
+    area?: {
+        type: string;
+        value: number;
+    }
+    description?: {
+        type: string;
+        value: string;
+    }
+    alternateName?: {
+        type: string;
+        value: string;
+    },
+    hasAgriCrop?: {
+        type: string;
+        object: string;
+    },
+    hasAgriSoil?: {
+        type: string;
+        object: string;
+    }
+    location?: {
+        type: string;
+        value: {
+            type: string;
+            features: [
+                {
+                    type: string;
+                    geometry: {
+                        type: string;
+                        coordinates: number[] | number[][][];
+                    }
+                    properties: {
+                        name: string;
+                    }
+                }
+            ]
+        };
+    };
+}
+
+export interface Device {
+    id: string;
+    type: string;
+    controlledAsset?: {
+        type: string;
+        value: string;
+    };
+    controlledProperty?: {
+        type: string
+        value: string;
+    };
+}
+
+export interface DeviceMeasurement {
+    id: string;
+    type: string;
+    numValue: number
+    refDevice: string;
+    dateObserved: {
+        type: string,
+        value: string
+    };
+}
+
+export interface QuantumLeapTimeSeriesData {
+    attributes: [
+        {
+            attrName: string,
+            values: string[] | number[],
+        }
+    ],
+    entityId: string,
+    entityType: string,
+    index: string[]
+}
+
+export interface AgriProductType {
+    id: string;
+    type: string;
+    name?: {
+        type: string;
+        value: string;
+    };
+    description?: {
+        type: string;
+        value: string;
+    };
+    alternateName?: {
+        type: string;
+        value: string;
+    };
+}
+
+
+export interface CommandMessage {
+    id: string,
+    type: string,
+    command: {
+        type: "string",
+        value: "string"
+    },
+    commandTime: {
+        type: "string",
+        value: "string"
+    },
+    waypoints: {
+        type: "string",
+        value: {
+            name: string,
+            type: string,
+            features: any[]
+        }
+    }
+}
+
+export interface StateMessage {
+    id: string,
+    type: string,
+    commandTime: string,
+    mode: string,
+    errors: any[],
+    pose: {
+        geographicPoint: {
+            latitude: number,
+            longitude: number,
+            altitude: number
+        },
+        orientation3D: {
+            roll: number,
+            pitch: number,
+            yaw: number
+        }
+    },
+    destination: {
+        geographicPoint: {
+            latitude: number,
+            longitude: number,
+            altitude: number
+        },
+        orientation3D: {
+            roll: number,
+            pitch: number,
+            yaw: number
+        }
+    },
+    accuracy: {
+        covariance: number[]
+    },
+    battery: {
+        remainingPercentage: number
+    }
+}
+
+export interface WeatherForecast {
+    id: string;
+    type: string;
+    description: string;
+    atmosphericPressure: number;
+    dateIssued: string;
+    dayMaximum: {
+        temperature: number;
+        feelLikesTemperature: number;
+        relativeHumidity: number;
+    };
+    dayMinimum: {
+        temperature: number;
+        feelLikesTemperature: number;
+        relativeHumidity: number;
+    };
+    feelLikesTemperature: number;
+    gustSpeed: number;
+    precipitation: number;
+    refPointOfInterest: string;
+    relativeHumidity: number;
+    temperature: number;
+    uVIndexMax: number;
+    visibility: number;
+    weatherType: string;
+    windDirection: number;
+    windSpeed: number;
+    dataProvider: string;
+    dateCreated: string;
+    dateModified: string;
+    location: {
+        type: string;
+        coordinates: [number, number];
+    };
+}
