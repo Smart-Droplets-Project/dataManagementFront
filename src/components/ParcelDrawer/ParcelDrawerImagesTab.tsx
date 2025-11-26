00fc98f4832ba9e9ff5ab8e7143d217e06f2444e
Mateo Budanovic <mateo.budanovic@vizlore.com>
2025-11-26 12:03:21 +0100

    installed react window and used it in the parcel drawer images tab to render the list of images

--- src/components/ParcelDrawer/ParcelDrawerImagesTab.tsx
+++ src/components/ParcelDrawer/ParcelDrawerImagesTab.tsx
@@ -1,17 +1,50 @@
-// import { Device, DeviceMeasurement, MeasurementExperiment } from "@/lib/interfaces";
 import { Device, DeviceMeasurement } from "@/lib/interfaces";
 import Grid from '@mui/material/Grid2';
-// import { Accordion, AccordionDetails, AccordionSummary, Skeleton, Typography } from "@mui/material";
 import { Skeleton } from "@mui/material";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
-// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Image from "next/image";
 
+import { List, useDynamicRowHeight, type RowComponentProps } from "react-window";
+
+type RowProps = {
+    images: {
+        lai: string[];
+        rbg: string[];
+    };
+};
+
+function RowComponent({ index, images, style }: RowComponentProps<RowProps>) {
+    const url1 = images.lai[index];
+    const url2 = images.rbg[index];
+
+    return (
+        <div style={style}>
+            <Grid display={'flex'} flexDirection={'row'} size={12} marginBottom={2}>
+                <Grid size={6} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                    <Image
+                        src={url1}
+                        alt="Experiment image 1"
+                        width={200}
+                        height={200}
+                        priority
+                    />
+                </Grid>
+                <Grid size={6} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                    <Image
+                        src={url2}
+                        alt="Experiment image 1"
+                        width={200}
+                        height={200}
+                        priority
+                    />
+                </Grid>
+            </Grid>
+        </div>
+    );
+}
 
 const ParcelDrawerImagesTab = (props: { selectedParcel: GeoJSON.Feature | null }) => {
     const { selectedParcel } = props;
-
-    // const [measurementExperiments, setMeasurementExperiments] = useState<MeasurementExperiment[] | null>(null);
 
     const [loading, setLoading] = useState(true);
 
@@ -21,38 +54,10 @@ const ParcelDrawerImagesTab = (props: { selectedParcel: GeoJSON.Feature | null }
     const [laiDeviceMeasurements, setLAIDeviceMeasurements] = useState<DeviceMeasurement[] | null>(null);
     const [rgbDeviceMeasurements, setRGBDeviceMeasurements] = useState<DeviceMeasurement[] | null>(null);
 
+    const [imageProps, setImageProps] = useState<RowProps>({ images: { lai: [], rbg: [] } });
 
 
     // On drawer open
-    // useEffect(() => {
-    //     console.log(selectedParcel);
-    //     if (selectedParcel) {
-    //         const fetchMeasurementExperiments = async () => {
-    //             try {
-    //                 const [res1] = await Promise.all([
-    //                     fetch(`/api/measurement-experiments`),
-
-    //                 ]);
-    //                 const data1 = await res1.json() as MeasurementExperiment[];
-    //                 setMeasurementExperiments(data1);
-
-    //                 console.log(data1);
-
-    //             } catch (err) {
-    //                 console.log(err);
-
-    //                 setError('Failed to load agri products');
-    //             } finally {
-    //                 setLoading(false)
-    //             }
-
-    //         }
-
-    //         setLoading(true)
-    //         fetchMeasurementExperiments();
-    //     }
-    // }, [selectedParcel])
-
     useEffect(() => {
         console.log(selectedParcel);
         if (selectedParcel) {
@@ -104,86 +109,36 @@ const ParcelDrawerImagesTab = (props: { selectedParcel: GeoJSON.Feature | null }
         }
     }, [devices])
 
-    // useEffect(() => {
-    //     if (laiDeviceMeasurements && rgbDeviceMeasurements) {
-    //         console.log(laiDeviceMeasurements);
-    //         console.log(rgbDeviceMeasurements);
-    //     }
-    // }, [laiDeviceMeasurements, rgbDeviceMeasurements])
+    useEffect(() => {
+        if (laiDeviceMeasurements && rgbDeviceMeasurements) {
+            const laiUrls = laiDeviceMeasurements.map((item: any) => item.alternateName.value);
+            const rgbUrls = rgbDeviceMeasurements.map((item: any) => item.alternateName.value);
+            setImageProps({
+                images: {
+                    lai: laiUrls,
+                    rbg: rgbUrls
+                }
+            });
+        }
+    }, [laiDeviceMeasurements, rgbDeviceMeasurements]);
+
+    const rowHeight = useDynamicRowHeight({
+        defaultRowHeight: 250
+    });
 
     return (
         <>
             {
                 loading ? <Skeleton height={"30vh"} width={"100%"} variant="rectangular"></Skeleton> :
                     error ? <p>{error}</p>
-                        // : !measurementExperiments ? <p>Measurement experiments fetching error</p>
-                        //     :
-                        //     <Grid container display={"flex"} flexDirection={"column"} size={12} spacing={3}>
-                        //         {
-                        //             measurementExperiments.map(m => {
-                        //                 return (
-                        //                     <Grid key={"grid-accordion-" + m.id} size={12}>
-                        //                         <Accordion>
-                        //                             <AccordionSummary
-                        //                                 expandIcon={<ExpandMoreIcon />}
-                        //                             >
-                        //                                 <Typography component="span">{m.name}</Typography>
-                        //                             </AccordionSummary>
-                        //                             <AccordionDetails>
-                        //                                 <Grid size={12} display={'flex'} flexDirection={'row'}>
-                        //                                     <Grid size={6} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                        //                                         <Image
-                        //                                             src="/images/Smart-droplets-logo.svg"
-                        //                                             alt="Experiment image 1"
-                        //                                             width={200}
-                        //                                             height={200}
-                        //                                             priority
-                        //                                         />
-                        //                                     </Grid>
-                        //                                     <Grid size={6} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                        //                                         <Image
-                        //                                             src="/images/Smart-droplets-logo.svg"
-                        //                                             alt="Experiment image 2"
-                        //                                             width={200}
-                        //                                             height={200}
-                        //                                             priority
-                        //                                         />
-                        //                                     </Grid>
-                        //                                 </Grid>
-                        //                             </AccordionDetails>
-                        //                         </Accordion>
-                        //                     </Grid>
-                        //                 )
-                        //             })
-                        //         }
-                        //     </Grid>
+
                         : !(laiDeviceMeasurements && rgbDeviceMeasurements) ? <p>images fetching error</p>
-                            : <Grid container display={"flex"} flexDirection={"column"} size={12} spacing={3}>
-                                {
-                                    laiDeviceMeasurements.map((m, index) => {
-                                        return <Grid key={"grid-row-" + m.id} display={'flex'} flexDirection={'row'} size={12}>
-                                            <Grid size={6} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                                                <Image
-                                                    src={m.alternateName.value}
-                                                    alt="Experiment image 1"
-                                                    width={200}
-                                                    height={200}
-                                                    priority
-                                                />
-                                            </Grid>
-                                            <Grid size={6} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-                                                <Image
-                                                    src={rgbDeviceMeasurements[index].alternateName.value}
-                                                    alt="Experiment image 1"
-                                                    width={200}
-                                                    height={200}
-                                                    priority
-                                                />
-                                            </Grid>
-                                        </Grid>
-                                    })
-                                }
-                            </Grid>
+                            : <List<RowProps>
+                                rowComponent={RowComponent}
+                                rowCount={laiDeviceMeasurements.length}
+                                rowHeight={rowHeight}
+                                rowProps={imageProps}
+                            />
             }
         </>
     )
